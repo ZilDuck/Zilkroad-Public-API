9ba8bbf979e87f9c7d90ae2d0bc16b6aa04a06b3
Elliott Green <elliottgreen@live.com>
2022-06-06 17:02:21 +0100

    [chore] fix collectionStatsController.getCollectionStats req.params

--- routes/collection-stats-route.js
+++ routes/collection-stats-route.js
@@ -2,6 +2,6 @@ const express = require('express')
 const collectionStatsController = require('../controllers/collection-stats-controller')
 const router = express.Router()
 
-router.get('/', collectionStatsController.getCollectionStats)
+router.get('/:contractAddress', collectionStatsController.getCollectionStats)
 
 module.exports = router
