5882cc814f8b9069674bcaa6be5dd1d108282c16
Elliott Green <eg145@brighton.ac.uk>
2022-08-15 20:52:52 +0100

    [bug] add param in route

--- controllers/order-transaction-controller.js
+++ controllers/order-transaction-controller.js
@@ -1,16 +1,16 @@
-const listing = require('../models/order-transaction')
+const orderTransaction = require('../models/order-transaction')
 
 module.exports = {
     getListedTransactionHashForOrderID: async function(req, res) {
     const orderID = req.params.orderID
 
-      let transactionHash = await order.getListedTransactionHashForOrderID(orderID)
+      let transactionHash = await orderTransaction.getListedTransactionHashForOrderID(orderID)
       res.send(transactionHash)
   },
     getSoldTransactionHashForOrderID: async function(req, res) {
         const orderID = req.params.orderID
 
-        let transactionHash = await order.getSoldTransactionHashForOrderID(orderID)
+        let transactionHash = await orderTransaction.getSoldTransactionHashForOrderID(orderID)
         res.send(transactionHash)
     }
 }
--- routes/order-transaction-route.js
+++ routes/order-transaction-route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const orderTransactionController = require('../controllers/order-transaction-controller')
 const router = express.Router()
 
-router.get('/listed', orderTransactionController.getListedTransactionHashForOrderID)
-router.get('/sold', orderTransactionController.getSoldTransactionHashForOrderID)
+router.get('/listed/:orderID', orderTransactionController.getListedTransactionHashForOrderID)
+router.get('/sold/:orderID', orderTransactionController.getSoldTransactionHashForOrderID)
 
 module.exports = router
