6761d933e295951efaf709ae65e7be57f70dbadb
Elliott Green <eg145@brighton.ac.uk>
2022-08-17 20:53:32 +0100

    [chore] make changes to model object

--- models/metadata.js
+++ models/metadata.js
@@ -5,14 +5,18 @@ const axios = require("axios");
 
 
 
-function Metadata(metadata_name, metadata_description, metadata_url, metadata_animation_url, collection_image_url)
+function Metadata(metadata_name, metadata_description, metadata_url, metadata_animation_url, 
+                  collection_image_url, discord, twitter, telegram)
 {
     return {
         name: metadata_name, 
         description: metadata_description, 
         url: metadata_url, 
         animation_url: metadata_animation_url,
-        collection_image_url: collection_image_url
+        collection_image_url: collection_image_url,
+        discord: discord,
+        twitter: twitter,
+        telegram: telegram
     }
 }
 
