41f32c2565a9ce5cf6699597a4334d2e766ab8f3
Elliott Green <eg145@brighton.ac.uk>
2022-09-21 20:20:27 +0100

    [feat] add order_id to api /nft

--- models/token.js
+++ models/token.js
@@ -174,13 +174,13 @@ async function getTokens(filter, limit, page, order, orderBy, query = {}) {
       db_result = await DBGetPaginatedListedTokensForContract(queried_contract, limit, page)
   }
 
-  const nfts = await Promise.all(db_result.map(async ({nonfungible_address, token_id, listing_fungible_token_price, fungible_symbol, decimals, verified}) => {
-
+    const nfts = await Promise.all(db_result.map(async ({static_order_id, nonfungible_address, token_id, listing_fungible_token_price, fungible_symbol, decimals, verified}) => {
     const contract_address_b16 = validation.isBech32(nonfungible_address) ? fromBech32Address(nonfungible_address) : nonfungible_address
     const contract_address_b32 = validation.isBech32(nonfungible_address) ? nonfungible_address : toBech32Address(nonfungible_address)
     const indexer_token = await indexer.GetTokenID(contract_address_b16, token_id).then(res => (res.data)).catch((error) => console.log(error)) // TODO Shouldn't have an api call in a loop like this. Need a batch method or listing data from indexer?
 
     return {
+      order_id: static_order_id,
       collection_name: indexer_token.name,
       symbol: indexer_token.symbol,
       contract_address_b16,
